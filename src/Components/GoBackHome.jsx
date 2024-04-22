b434aadb6cce16caf92cdd62ede690f57a3ce240
Abdul Muhaimin Toha <abdulmuhaimintoha.business@gmail.com>
2024-04-22 20:14:53 +0600

    add new coffee function done

--- src/Components/GoBackHome.jsx
+++ src/Components/GoBackHome.jsx
@@ -1,11 +1,12 @@
 import { IoIosArrowRoundBack } from "react-icons/io";
+import { Link } from "react-router-dom";
 
 const GoBackHome = () => {
   return (
-    <div className="my-10  flex items-center gap-2 text-2xl">
+    <Link to="/" className="my-10  flex items-center gap-2 text-2xl">
       <IoIosArrowRoundBack />
       <p className="font-serif text-3xl">Back to home</p>
-    </div>
+    </Link>
   );
 };
 
--- src/Components/PopularProducts.jsx
+++ src/Components/PopularProducts.jsx
@@ -1,6 +1,7 @@
 import ProductCard from "./ProductCard";
 import SectionHeader from "./SectionHeader";
 import coffeIcon from "../assets/service-1.png";
+import { Link } from "react-router-dom";
 
 const PopularProducts = () => {
   return (
@@ -11,10 +12,13 @@ const PopularProducts = () => {
           subHeading="--- Sip & Savor ---"
         />
         <div className="mb-10 mt-10 flex items-center justify-center">
-          <button className="flex items-center justify-center gap-2 rounded-md bg-primary px-4 py-3 font-serif text-lg text-white">
+          <Link
+            to="/coffee-add-new"
+            className="flex items-center justify-center gap-2 rounded-md bg-primary px-4 py-3 font-serif text-lg text-white"
+          >
             <p>Add Coffee</p>
             <img src={coffeIcon} alt="Icon of Coffee" className="w-8" />
-          </button>
+          </Link>
         </div>
         <div className="mb-10 grid grid-cols-1 gap-6 md:mb-16 md:grid-cols-2 lg:grid-cols-3">
           <ProductCard />
